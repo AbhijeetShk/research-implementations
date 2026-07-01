91db28e13d8954dac61dda2766a1f1c828486a5f
AbhijeetShk <programmerdostofficial@gmail.com>
2026-07-01 23:25:38 +0530

    feat(exominer): implement dataset and dataloader pipeline

--- exominer/src/dataset.py
+++ exominer/src/dataset.py
@@ -1,5 +1,69 @@
+import torch
 from torch.utils.data import Dataset
 
 
 class ExoMinerDataset(Dataset):
-    pass
+
+    def __init__(
+        self,
+        full_flux,
+        transit_flux,
+        full_centroid,
+        transit_centroid,
+        scalar_features,
+        labels
+    ):
+        self.full_flux = full_flux
+        self.transit_flux = transit_flux
+
+        self.full_centroid = full_centroid
+        self.transit_centroid = transit_centroid
+
+        self.scalar_features = scalar_features
+
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+
+        sample = {
+            "full_flux":
+                torch.tensor(
+                    self.full_flux[idx],
+                    dtype=torch.float32
+                ),
+
+            "transit_flux":
+                torch.tensor(
+                    self.transit_flux[idx],
+                    dtype=torch.float32
+                ),
+
+            "full_centroid":
+                torch.tensor(
+                    self.full_centroid[idx],
+                    dtype=torch.float32
+                ),
+
+            "transit_centroid":
+                torch.tensor(
+                    self.transit_centroid[idx],
+                    dtype=torch.float32
+                ),
+
+            "scalar_features":
+                torch.tensor(
+                    self.scalar_features[idx],
+                    dtype=torch.float32
+                ),
+
+            "label":
+                torch.tensor(
+                    self.labels[idx],
+                    dtype=torch.float32
+                )
+        }
+
+        return sample
